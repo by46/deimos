9bb01c80378e30bf0835600ad9551eba149d9796
benjamin.c.yan <benjamin.c.yan@newegg.com>
2016-07-26 09:30:24 +0800

    add flask restful

--- ci_build.sh
+++ ci_build.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-docker run --rm -v ${WORKSPACE}:/home/matrix -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock docker.neg/matrix:0.0.3 /usr/local/bin/matrix.sh
+sirius docker_build_image
+
+# failure with non-zero return code
+[ $? -gt 0 ] && exit 1
+
+IMAGE=$(sirius docker_image_name | head -n 1)
+
+docker tag -f ${IMAGE} docker.neg/${IMAGE}
+
+# failure with non-zero return code
+[ $? -gt 0 ] && exit 1
+
+docker push docker.neg/${IMAGE}
+
+# failure with non-zero return code
+[ $? -gt 0 ] && exit 1
+
+exit 0
--- ci_deploy.sh
+++ ci_deploy.sh
@@ -2,8 +2,4 @@
 
 IMAGE=$(sirius docker_image_name | head -n 1)
 
-docker tag -f ${IMAGE} docker.neg/${IMAGE}
-
-docker push docker.neg/${IMAGE}
-
 sirius docker_deploy:deimos,${IMAGE},server=scdfis01,ports="9200;8080"
